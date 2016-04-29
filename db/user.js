9778149025b78b67e84e40766fad0dec8683ca99
coding-happily <834102224@qq.com>
2016-04-29 10:36:17 +0800

    新增登录界面，增加判断语句

--- db/user.js
+++ db/user.js
@@ -1,17 +1,22 @@
 var mongodb = require("./mongodb");
 var Schema = mongodb.mongoose.Schema;
 var userSchema = new Schema({
-  nickname: String,
+  nickname: {type:String,unique:true},
   password: String,
 });
 
-userSchema.static.findByNickname = function(nickname,callback){
-  return this.model('mongoose').find({nickname: nickname},callback);
-}
+// userSchema.method.findByNickname = function(nickname,callback){
+//   return this.model('mongoose').find({nickname: nickname},callback);
+// }
+//根据Nickname查找
+userSchema.static('findByNickname',function (nickname,callback) {
+  return this.find({nickname: nickname},callback);
+})
 
-userSchema.static.checkPassword = function(password,callback){
-  // return this.model('mongoose').find({});
-  return 1;
-}
+// userSchema.static.checkPassword = function(password,callback){
+//   // return this.model('mongoose').find({});
+//   return 1;
+// }
 // var User = mongodb.mongoose.model("User",userSchema);
 exports.User = mongodb.mongoose.model("User",userSchema);
+// module
--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var User = require("./db/user.js").User;
 
 var app = express();
 
+var account = {};//账号记录
+
 app.set("views",__dirname+"/views");
 app.engine('.html', require('ejs').__express);
 app.set("view engine","html");
@@ -39,17 +41,25 @@ app.get("/signin",function(req,res){
 
 //注册记录
 app.post("/signin",function(req,res){
-  console.log("nickname: "+req.body.nickname);
-  console.log("password: "+req.body.password);
+  // console.log("nickname: "+req.body.nickname);
+  // console.log("password: "+req.body.password);
   //生成密码的 md5 值
   var md5 = crypto.createHash('md5');
   var password = md5.update(req.body.password).digest('hex');
-  console.log("passwordHex:"+password);
-  //存储数据到数据库
-  var newUser = new User({
-    nickname: req.body.nickname,
-    password: password,
+  // console.log("passwordHex:"+password);
+  account["nickname"] = req.body.nickname;
+  account["password"] = password;
+  console.log(account);
+  User.findByNickname(account["nickname"],function(err,user){
+    if(err){
+      console.log(err);
+    }
   });
+  //存储数据到数据库
+  // var newUser = new User({
+  //   nickname: req.body.nickname,
+  //   password: password,
+  // });
   
   newUser.save();
   res.redirect("/index");
@@ -60,11 +70,34 @@ app.post("/signin",function(req,res){
 
 //登录界面
 app.get('/login',function(req,res){
-  
+  res.render("login");
 });
 
 app.post('/login',function(req,res){
-  
+  // console.log(req.body.nickname);
+  // console.log(req.body.password);
+  //生成密码的 md5 值
+  var md5 = crypto.createHash('md5');
+  var password = md5.update(req.body.password).digest('hex');
+  // console.log("passwordHex:"+password);
+  account["nickname"] = req.body.nickname;
+  account["password"] = password;
+  console.log(account);
+  //登录账户查询，是否存在该用户
+  User.findByNickname(account["nickname"],function(err,users){
+    if(err){
+      console.log(err);
+    }
+    console.log(users);
+    if(!users.length){
+      console.log("该用户不存在！请先注册")
+      res.json({msg:"0"});//不存在则返回字符0
+    }else{
+      console.log("欢迎你，"+account["nickname"]+" !");
+      res.json({msg:"1"});
+    }
+  });
+  //res.redirect('/login');
 });
 
 http.createServer(app).listen(8888);
